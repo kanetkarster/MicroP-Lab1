4a696c5bb6a692187f4314ab38ffcf17a4b1f1b1
unknown <skanet1@156TR4160MP-14.campus.MCGILL.CA>
2015-09-27 17:57:57 -0400

    updated API for C function

--- source/main.c
+++ source/main.c
@@ -12,19 +12,42 @@ typedef struct {
 } HMM;
 
 // Assembly Functions
-extern int ViterbiUpdate_asm(HMM *model);
-
+extern int ViterbiUpdate_asm(float* viterbi_in, float* viterbi_out, float obs, HMM *model);
+int ViterbiUpdate_c(float* viterbi_in, float* viterbi_out, float obs, HMM *model);
 int main()
 {	
-	HMM model;
-	model.S = 3;
-	model.V = 12;
-	model.transition = malloc(model.S * model.S * sizeof(float));
-	model.emission = malloc(model.S * model.V * sizeof(float));
-	model.prior = malloc(model.S * sizeof(float));
+	int S = 4, V = 10;
+	// VITPSI
+	float* vitpsi_i = malloc(2*S*sizeof(float));
+	float* vitpsi_o = malloc(2*S*sizeof(float));
 	
-	int s = ViterbiUpdate_asm(&model);
+	float OBS = 2;
+	
+	// Hidden Markov Model
+	HMM model;
+	model.S = S;
+	model.V = V;
+	model.transition = malloc(S * S * sizeof(float));
+
+	model.emission = malloc(S * V * sizeof(float));
+	model.prior = malloc(S * sizeof(float));
+
+	int i, j;
+	for (i=0; i < S; i++) {
+		for (j=0; j < S; j++) {
+			model.transition[i][j] = (float)rand()/(float)(RAND_MAX);
+		}
+		for (j=0; j < V; j++) {
+			model.emission[i][j] = (float)rand()/(float)(RAND_MAX);
+		}
+		model.prior[i] = (float)rand()/(float)(RAND_MAX);
+	}
+	int s = ViterbiUpdate_asm(vitpsi_i, vitpsi_o, OBS, &model);
 	
 	printf("%d\n", s);
+	return 0;
+}
+
+int ViterbiUpdate_c(float* viterbi_in, float* viterbi_out, float obs, HMM *model) {
 	return 0;
 }
