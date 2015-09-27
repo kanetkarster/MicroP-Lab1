e972d4bd22802b50b8461cfdf44df24a7256ebde
unknown <skanet1@156TR4160MP-14.campus.MCGILL.CA>
2015-09-27 17:58:22 -0400

    added viterbi algorithm in C

--- source/main.c
+++ source/main.c
@@ -13,7 +13,7 @@ typedef struct {
 
 // Assembly Functions
 extern int ViterbiUpdate_asm(float* viterbi_in, float* viterbi_out, float obs, HMM *model);
-int ViterbiUpdate_c(float* viterbi_in, float* viterbi_out, float obs, HMM *model);
+int ViterbiUpdate_c(float* viterbi_in, float* viterbi_out, int obs, HMM *model);
 int main()
 {	
 	int S = 4, V = 10;
@@ -42,12 +42,35 @@ int main()
 		}
 		model.prior[i] = (float)rand()/(float)(RAND_MAX);
 	}
-	int s = ViterbiUpdate_asm(vitpsi_i, vitpsi_o, OBS, &model);
+	int s = ViterbiUpdate_c(vitpsi_i, vitpsi_o, OBS, &model);
 	
 	printf("%d\n", s);
 	return 0;
 }
 
-int ViterbiUpdate_c(float* viterbi_in, float* viterbi_out, float obs, HMM *model) {
+int ViterbiUpdate_c(float* viterbi_in, float* viterbi_out, int obs, HMM *model) {
+	float max = -10000;
+	int max_index;
+	float sum = 0;
+	for (int s = 0; s < model->S; s++) {
+		for (int j = 0; j < 2*model->S; j++) {
+			float* transmitted = model->transition[j] + j*sizeof(&model->transition);
+			float val = viterbi_in[j] * *transmitted;
+			sum += val;
+			if (val > max) {
+				max = val;
+				max_index = j;
+			}
+		}
+		// fill vit
+		viterbi_out[2*s] = max;
+		// fill psi
+		viterbi_out[2*s+1] = ((float)max_index ) * model->emission[s][obs];
+	}
+	float scale = 1.0f / sum;
+	for (int s = 0; s < model->S; s++) {
+		viterbi_out[2*s] = scale * viterbi_out[s];
+	}
+	
 	return 0;
 }
