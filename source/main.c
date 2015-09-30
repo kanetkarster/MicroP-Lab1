6df8e49cc46a4329b7538e578c15f0be796726af
Jit <kanetkarster@gmail.com>
2015-09-30 12:58:10 -0400

    - fixed printing -rewerote Viterbi Update -changed constants

--- source/main.c
+++ source/main.c
@@ -13,7 +13,7 @@ typedef struct {
 	int V;
 	float transition[N_STATES][N_STATES];
 	float emission[N_STATES][N_OBS];
-	float* prior;
+	float prior[N_STATES];
 } HMM;
 
 // Assembly Functions
@@ -22,8 +22,8 @@ int ViterbiUpdate_c(float* viterbi_in, float* viterbi_out, int obs, HMM *model);
 int main()
 {	
 	// VITPSI
-	float vitpsi_i[N_STATES];
-	float vitpsi_o[N_STATES];
+	float vitpsi_i[2*N_STATES];
+	float vitpsi_o[2*N_STATES];
 	
 	float EMMISSION[N_STATES][N_OBS]= {1.0f, 2.0f};
 	float TRANSMISSION[N_STATES][N_STATES]= {{1.0f, 2.0f},
@@ -41,15 +41,15 @@ int main()
 		for (int j = 0; j < N_OBS; j++) {
 			model.emission[i][j] = EMMISSION[i][j];
 		}
-		vitpsi_i[2*i] = .3 * i;
-		vitpsi_i[2*i+1] = .25 * i;
+		vitpsi_i[2*i] = .5;
+		vitpsi_i[2*i+1] = .5;
 	}
 	
 
 	int s = ViterbiUpdate_c(vitpsi_i, vitpsi_o, OBS, &model);
 	
 	for (int i = 0; i < N_STATES; i++) {
-		printf("psi: %3.3f\t vit %3.3f\n", vitpsi_o[i], vitpsi_o[i+1]);
+		printf("psi: %3.3f\t vit %3.3f\n", vitpsi_o[2*i], vitpsi_o[2*i+1]);
 	}
 	
 	printf("%d\n", s);
@@ -57,26 +57,33 @@ int main()
 }
 
 int ViterbiUpdate_c(float* viterbi_in, float* viterbi_out, int obs, HMM *model) {
-	float max = -10000;
-	int max_index;
-	float sum = 0;
-	for (int s = 0; s < model->S; s++) {
-		for (int j = 0; j < 2*model->S; j++) {
-			float val = viterbi_in[j] * model->transition[j][s];
-			sum += val;
-			if (val > max) {
-				max = val;
+	float trans_p[N_STATES];
+	float max = -1000;
+	int max_index = -1;
+	for (int i = 0; i < model->S; i++) {
+		for (int j = 0; j < model->S; j++) {
+			trans_p[j] = viterbi_in[j] * model->transition[i][j];
+		}
+		for (int j = 0; j < model->S; j++) {
+			if (trans_p[j] > max) {
+				max = trans_p[j];
 				max_index = j;
 			}
 		}
-		// fill vit
-		viterbi_out[2*s] = max;
-		// fill psi
-		viterbi_out[2*s+1] = ((float)max_index ) * model->emission[s][obs];
+		viterbi_out[2*i] = max;						// assign vit (value)
+		viterbi_out[2*i + 1] = max_index; // assign psi (index)
+		viterbi_out[2*i] = max * obs;
 	}
-	float scale = 1.0f / sum;
-	for (int s = 0; s < model->S; s++) {
-		viterbi_out[2*s] = scale * viterbi_out[s];
+	
+	float sum = 0;
+	for (int i = 0; i < model->S; i++) {
+		sum += viterbi_out[2*i];
+	}
+	
+	float scale = 1.0f/sum;
+	
+	for (int i = 0; i < model->S; i++) {
+		viterbi_out[2*i] = viterbi_out[2*i] * scale;
 	}
 	
 	return 0;
