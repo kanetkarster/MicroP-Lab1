d65fe673a2be276bdcb492ea987f17b3c2e7011c
Jit <kanetkarster@gmail.com>
2015-09-30 16:18:56 -0400

    need to get C viterbi to work

--- source/main.c
+++ source/main.c
@@ -3,9 +3,12 @@
 #include "arm_math.h"
 
 #define N_STATES 2
-#define N_OBS 1
+#define N_OBS 3
 
 #define RAND() (float)rand()/(float)(RAND_MAX)
+
+static char *PRINT_STATE[2] = {"HEALTHY",
+															 "FEVER"};
 
 // typedefs
 typedef struct {
@@ -26,12 +29,13 @@ int main()
 	// output
 	int EstimatedStates[N_OBS];
 	// initial values for HMM
-	float EMMISSION[N_STATES][N_OBS]= {1.0f, 2.0f};
-	float TRANSMISSION[N_STATES][N_STATES]= {{1.0f, 2.0f},
-																					 {31.0f, 4.0f}};
-	float PRIOR[N_STATES] = {.25, .5};
-	int Observations[N_OBS] = {1};
-	
+	float EMMISSION[N_STATES][N_OBS]= {{0.5f, 0.4f, 0.1f},
+																		 {0.1f, 0.3f, 0.6f}};
+
+	float TRANSMISSION[N_STATES][N_STATES]= {{0.7f, 0.3f},
+																					 {0.4f, 0.6f}};
+	float PRIOR[N_STATES] = {.6, .4};
+	int Observations[N_OBS] = {0, 1, 2};
 	// Fill Hidden Markov Model
 	HMM model;
 	model.S = N_STATES;
@@ -57,7 +61,9 @@ int main()
 int Viterbi_C(int* Observations, int Nobs, int* EstimatedStates, HMM* hmm) {
 	float states[Nobs+1][2*N_STATES];
 	states[0][0] = hmm->prior[0];
-	states[0][1] = hmm->prior[1];
+	states[0][1] = 0;
+	states[0][2] = hmm->prior[1];
+	states[0][3] = 1;
 	
 	for (int i = 0; i < Nobs; i++) {
 		ViterbiUpdate_c(states[i], states[i+1], Observations[i], hmm);
@@ -70,17 +76,17 @@ int Viterbi_C(int* Observations, int Nobs, int* EstimatedStates, HMM* hmm) {
 			EstimatedStates[i] = (int) states[N_OBS][2*i+1];
 		}
 	}
-	//printf("Ends at:\n");
+	printf("Max Prob: %3.3f\n", prob);
 	return 0;
 }
 int ViterbiUpdate_c(float* viterbi_in, float* viterbi_out, int obs, HMM *model) {
-	float trans_p[N_STATES];
-	float max = -1000;
 	int max_index = -1;
+	float trans_p[N_STATES];
 	for (int i = 0; i < model->S; i++) {
 		for (int j = 0; j < model->S; j++) {
-			trans_p[j] = viterbi_in[j] * model->transition[i][j];
+			trans_p[j] = viterbi_in[2*j] * model->transition[i][j];
 		}
+		float max = -1000;
 		for (int j = 0; j < model->S; j++) {
 			if (trans_p[j] > max) {
 				max = trans_p[j];
@@ -88,8 +94,9 @@ int ViterbiUpdate_c(float* viterbi_in, float* viterbi_out, int obs, HMM *model) 
 			}
 		}
 		viterbi_out[2*i] = max;						// assign vit (value)
+		printf("%s: %3.3f\n", PRINT_STATE[i], max);
 		viterbi_out[2*i + 1] = max_index; // assign psi (index)
-		viterbi_out[2*i] = max * obs;
+		viterbi_out[2*i] = max * model->emission[i][obs];
 	}
 	
 	float sum = 0;
