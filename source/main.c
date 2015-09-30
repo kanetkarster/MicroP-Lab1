0bc26f63fb8903664b9f439cc71ad8c3e36bcd81
Jit <kanetkarster@gmail.com>
2015-09-30 17:00:06 -0400

    tried test case from classmate in assembly

--- source/main.c
+++ source/main.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 #include "arm_math.h"
 
-#define N_STATES 2
-#define N_OBS 3
+#define N_STATES 3
+#define N_OBS 4
 
 #define RAND() (float)rand()/(float)(RAND_MAX)
 
@@ -30,13 +30,15 @@ int main()
 	// output
 	int EstimatedStates[N_OBS];
 	// initial values for HMM
-	float EMMISSION[N_STATES][N_OBS]= {{0.5f, 0.4f, 0.1f},
-																		 {0.1f, 0.3f, 0.6f}};
+	float EMMISSION[N_STATES][N_OBS]= {{2.0f, 2.0f, 3.0f, 1.0f},
+																		 {1.0f, 1.0f, 2.0f, 2.0f},
+																		 {2.0f, 1.0f, 2.0f, 1.0f}};
 
-	float TRANSMISSION[N_STATES][N_STATES]= {{0.7f, 0.3f},
-																					 {0.4f, 0.6f}};
-	float PRIOR[N_STATES] = {.6, .4};
-	int Observations[N_OBS] = {0, 1, 2};
+	float TRANSMISSION[N_STATES][N_STATES]= {{1.0f, 1.0f, 2.0f},
+																					 {5.0f, 1.0f, 2.0f},
+	                                         {4.0f, 3.0f, 1.0f}};
+	float PRIOR[N_STATES] = {1.0f, 2.0f, 2.0f};
+	int Observations[N_OBS] = {2, 2, 2};
 	// Fill Hidden Markov Model
 
 	HMM model;
@@ -63,15 +65,16 @@ int main()
 int Viterbi_C(int* Observations, int Nobs, int* EstimatedStates, HMM* hmm) {
 	float states[Nobs+1][2*N_STATES];
 	states[0][0] = hmm->prior[0];
-	states[0][1] = 0;
+	states[0][1] = 12;
 	states[0][2] = hmm->prior[1];
-	states[0][3] = 1;
-	
+	states[0][3] = 12;
+	states[0][3] = hmm->prior[2];
+	states[0][3] = 12;
 	for (int i = 0; i < Nobs; i++) {
 		ViterbiUpdate_asm(states[i], states[i+1], Observations[i], hmm);
-		for (int j = 0; j < N_STATES; j++) {
-			printf("vit: %f\tpsi: %f\n", states[i+1][2*j]);
-		}
+		//for (int j = 0; j < N_STATES; j++) {
+		//	printf("vit: %f\tpsi: %f\n", states[i+1][2*j], states[i+1][2*j+1]);
+		//}
 	}
 	
 	float prob = -1;
